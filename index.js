e434c1e0f80a25d211c092fe4f28ce371382aed1
Trey Wayland <waylandtrey7@gmail.com>
2024-07-29 21:49:25 -0700

    Refactored Code.

--- index.js
+++ index.js
@@ -70,7 +70,6 @@ function init() {
 // Function call to initialize app
 init();
 
-
 function askUserQuestions(){
     inquirer
   .prompt(questions)
@@ -81,65 +80,7 @@ function askUserQuestions(){
 
 
 function generateReadMe(response){
-
-    // let licenseURL = getLicenseURL(response.License);
-
     let genMarkdownText = generateMarkdown(response);
 
-//     let readMeString = 
-//     `# ${response.projectTitle}
-// ${licenseURL}
-
-// ## Description
-// ${response.description} \n
-
-
-// ## Table of Contents
-// * [Installation](#installation)
-// * [Usage](#usage)
-// * [License](#license)
-// * [Contributing](#contributing)
-// * [Tests](#tests)
-// * [Questions](#questions)
-
-// ## Installation
-// ${response.installInstructs} \n
-
-// ## Usage
-// ${response.usageInformation} \n
-
-// ## License
-// This application is using the ${response.License} License.\n
-
-// ## Contributing
-// ${response.contributed} \n
-
-// ## Tests
-// ${response.testInstructions} \n
-
-// ## Questions
-// * The link to the github repository can be found here: [${response.gitHubUsername}](https://github.com/${response.gitHubUsername}/)
-// * For any further questions you can reach out to: ${response.emailAddress}
-// `;
-    
     writeToFile("README.md", genMarkdownText) ;
-}
-
-function getLicenseURL(license){
-
-
-    switch (license){
-        case "MIT":
-            return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-        case "GNU":
-            return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-        case "IBM":
-            return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
-        case "Apache License 2.0":
-            return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-        case "Perl":
-            return "[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)";
-        case "Eclipse":
-            return "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
-    }
 }
